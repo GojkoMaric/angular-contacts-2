810c3567da19ccadbc71d864a2ddba0aff81e21f
Danilo Banjanin <danilo216@gmail.com>
2017-08-23 15:24:55 +0200

    Commit 18 - Extract contacts list to separate file

---
+++ src/app/components/contacts/contacts.ts
@@ -0,0 +1,22 @@
+export let contacts = [
+  {
+    firstName: 'John',
+    lastName:  'Doe',
+    email:     'john@example.com'
+  },
+  {
+    firstName: 'Daniel',
+    lastName:  'Ros',
+    email:     'daniel@example.com'
+  },
+  {
+    firstName: 'Martin',
+    lastName:  'Hess',
+    email:     'martin@example.com'
+  },
+  {
+    firstName: 'John',
+    lastName:  'Deere',
+    email:     'john.deere@example.com'
+  },
+];
