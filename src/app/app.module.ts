87a9b0b985b9f757f2d4916f5cdf2b77f1a59616
Danilo Banjanin <danilo216@gmail.com>
2017-08-23 14:14:18 +0200

    Commit 13 - Create messages component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
     LayoutComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule
