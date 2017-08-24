63cafcfdf417343ae7bdce5aaaeff502120f8b17
Dejan Stokic <dejan.s@vivifyideas.com>
2017-08-24 09:54:56 +0200

    Fix - Changed custom filter pipe to search both in first and last name.

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -2,7 +2,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'filter' })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], field: string, value: string) {
-    return items.filter(item => item[field].indexOf(value) !== -1);
+  transform(items: any[], fields: string[], value: string) {
+    return items.filter(function (item) {
+      let source = '';
+      let needle = value.trim().toLocaleLowerCase();
+
+      fields.forEach(function (field) {
+        source += item[field].toLowerCase() + ' ';
+      });
+
+      return source.indexOf(needle) !== -1;
+    });
   }
 }
